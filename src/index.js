9e552bf2904d24c05afdcf11fb14d569f45e3518
mqtent <mqtent@gmail.com>
2019-09-17 14:26:48 +0700

    forkJoin

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-import { of, from } from 'rxjs'; 
-import { map, delay, mergeMap, concatMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+import { forkJoin } from 'rxjs';
 
-const getData = (param) => {
-  const delayTime = Math.floor(Math.random() * 10000) + 1;
-  return of(`retrieved new data with params: ${param} and delay: ${delayTime}`).pipe(
-    delay(delayTime)
-  )
-}
-
-// using a regular map
-from([1,2,3,4]).pipe(
-  map(param => getData(param))
-).subscribe(val => val.subscribe(data => console.log('map:', data)));
-
-// using mergeMap
-from([1, 2, 3 ,4]).pipe(
-  mergeMap(param => getData(param))
-).subscribe(val => console.log('mergeMap:', val));
-
-// using concatMap (emit in order of index)
-from([1, 2, 3 ,4]).pipe(
-  concatMap(param => getData(param))
-).subscribe(val => console.log('concatMap:', val));
+/*
+  when all observables complete, provide the last
+  emitted value from each as dictionary
+*/
+forkJoin(
+  // as of RxJS 6.5+ we can use a dictionary of sources
+  {
+    google: ajax.getJSON('https://api.github.com/users/google'),
+    microsoft: ajax.getJSON('https://api.github.com/users/microsoft'),
+    users: ajax.getJSON('https://api.github.com/users')
+  }
+)
+// { google: object, microsoft: object, users: array }
+.subscribe(console.log);
