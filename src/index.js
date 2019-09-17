a948f5268b87f7ff8f8bca655a905c231c4aa110
mqtent <mqtent@gmail.com>
2019-09-17 14:08:40 +0700

    mergeMap vs concatMap

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import { of, from } from 'rxjs'; 
-import { map, mergeMap, delay, mergeAll } from 'rxjs/operators';
+import { map, delay, mergeMap, concatMap } from 'rxjs/operators';
 
 const getData = (param) => {
-  return of(`retrieved new data with param ${param}`).pipe(
-    delay(1000)
+  const delayTime = Math.floor(Math.random() * 10000) + 1;
+  return of(`retrieved new data with params: ${param} and delay: ${delayTime}`).pipe(
+    delay(delayTime)
   )
 }
 
-// using a regular map (Have to subscribe 2 times)
+// using a regular map
 from([1,2,3,4]).pipe(
   map(param => getData(param))
-).subscribe(val => val.subscribe(data => console.log(data)));
+).subscribe(val => val.subscribe(data => console.log('map:', data)));
 
-// using map and mergeAll (Have to add mergeAll)
-from([1,2,3,4]).pipe(
-  map(param => getData(param)),
-  mergeAll()
-).subscribe(val => console.log(val));
-
-// using mergeMap (The best way)
-from([1,2,3,4]).pipe(
+// using mergeMap
+from([1, 2, 3 ,4]).pipe(
   mergeMap(param => getData(param))
-).subscribe(val => console.log(val));
+).subscribe(val => console.log('mergeMap:', val));
+
+// using concatMap (emit in order of index)
+from([1, 2, 3 ,4]).pipe(
+  concatMap(param => getData(param))
+).subscribe(val => console.log('concatMap:', val));
